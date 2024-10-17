de5b5d1ebd3b1dff834e90ea2f3999b9a543bc22
andreas <andrklae@uio.no>
2024-10-17 19:03:17 +0200

    change å to a in files

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ let underConstruction = true
 
 if(underConstruction){
     body = document.getElementById("body")
-    document.body.style.backgroundImage = "url('bilder/forside/ålesund.jpeg')";
+    document.body.style.backgroundImage = "url('bilder/forside/alesund.jpeg')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundPosition = "center";
     //document.body.style.backgroundRepeat = "no-repeat";
